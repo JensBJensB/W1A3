4a1956a5d761d5a22af2c6730910b0cf8747f4ec
Jens <11554129+JensBJensB@users.noreply.github.com>
2021-11-26 16:55:05 +0100

    Add project files.

---
+++ W1A3/Controllers/GuessNumberController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using W1A3.Models;
+
+namespace W1A3.Controllers
+{
+    public class GuessNumberController : Controller
+    {
+        public static int GenerateNumber()
+        {
+            var NumberToGuess = new Random();
+            return NumberToGuess.Next(1, 100);
+        }
+
+        [HttpGet]
+        public ActionResult GTN()
+        {
+
+            HttpContext.Session.SetInt32("NumberToGuess", GenerateNumber());
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GTN(int guessedNumber)
+        {
+            GuessNumberModel gnp = new GuessNumberModel();
+            
+            gnp.NumberToGuess = HttpContext.Session.GetInt32("NumberToGuess").Value;
+            gnp.GuessedNumber = guessedNumber;
+            ViewBag.Message = gnp.CheckGuess(guessedNumber);
+            return View();
+        }
+    }
+}
+
+
---
+++ W1A3/Models/GuessNumberModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W1A3.Models
+{
+    public class GuessNumberModel
+    {
+        [ViewData]
+        public int NumberToGuess { get; set; }
+
+        [Required]
+        [Range(0, 101, ErrorMessage = "The number is between 1 and 100."), MaxLength(3)]
+        public int GuessedNumber { get; set; }
+
+
+        public static string WriteMessage()
+        {
+            return "test";
+        }
+
+        public string CheckGuess(int guessedNumber)
+        {
+            string message;
+
+            if (guessedNumber == NumberToGuess)
+            {
+                message = "You guessed correctly";
+            }
+            else if (guessedNumber > NumberToGuess)
+            {
+                message = ("The number is smaller");
+            }
+            else
+            {
+                message = ("Your number is larger");
+            }
+            return message;
+        }
+    }
+}
+
+
+
+// kika på detta senare
+// https://docs.microsoft.com/en-us/dotnet/api/system.random?redirectedfrom=MSDN&view=net-6.0#ThreadSafety
